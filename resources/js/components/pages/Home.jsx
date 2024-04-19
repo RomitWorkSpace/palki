fa64779996f7c0778e609cc4662fbf85bd14d8a4
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-19 10:25:44 +0530

    all section added to homepage

--- resources/js/components/pages/Home.jsx
+++ resources/js/components/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import HeroSection from '../HomeComponents/HeroSection'
 import DishSection from '../HomeComponents/DishSection'
 import ServiceSection from '../HomeComponents/ServiceSection'
+import ContactSection from '../HomeComponents/ContactSection'
+import StorySection from '../HomeComponents/StorySection'
+import PopularFood from '../HomeComponents/PopularFood'
+import TeamSection from '../HomeComponents/TeamSection'
+import BlogSection from '../HomeComponents/BlogSection'
 
 function Home() {
     return (
@@ -9,6 +14,11 @@ function Home() {
         <HeroSection />
         <DishSection />
         <ServiceSection />
+        <ContactSection />
+        <StorySection />
+        <PopularFood />
+        <TeamSection />
+        <BlogSection />
         </>
     )
 }

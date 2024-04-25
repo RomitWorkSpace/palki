fb71a90594d65e5683aaec0f86f90850787ea7bf
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-25 11:47:39 +0530

    all pages added

--- resources/js/common/Layout.jsx
+++ resources/js/common/Layout.jsx
@@ -3,6 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from '../components/pages/Home';
 import Header from './Header';
 import Footer from './Footer';
+import About from '../components/pages/About';
+import Contact from '../components/pages/Contact';
+import Blog from '../components/pages/Blog';
+import Service from '../components/pages/Service';
+import ServiceDetail from '../components/pages/ServiceDetail';
+import Booking from '../components/pages/Booking';
 
 
 function Layout() {
@@ -11,8 +17,13 @@ function Layout() {
     <Header />
     <Routes>
         <Route path="/" exact element={<Home/>} />
-        {/* <Route path="/about" exact element={<About/>} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
         <Route path="/blog" exact element={<Blog />} />
+        <Route path="/services" exact element={<Service />} />
+        <Route path="/service-detail" exact element={<ServiceDetail />} />
+        <Route path="/share-query" exact element={<Booking />} />
+        {/* <Route path="/about" exact element={<About/>} />
         <Route path="/hostel" exact element={<HostelRoom />} />
         <Route path="/room-detail" exact element={<RoomDetail />} />
         <Route path='/facilities/:slug' exact element={<Service />} />

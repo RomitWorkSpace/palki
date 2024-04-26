8db90027cd5d85ae65a7fd52d2bb67e8895789e5
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-26 12:42:57 +0530

    Our team card created

---
+++ resources/js/components/HomeComponents/TeamCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TeamCard() {
+    return (
+        <>
+            <div className='team-wrapper'>
+                <img src="https://kamleshyadav.com/html/catering/html/assets/images/main/team/01.jpg" alt="chef" width="100%" />
+                <div className='team-identity'>
+                    <h6>Decoration Chef</h6>
+                </div>
+                <div className='team-info'>
+                    <h5 className=''>Jenny Disusa</h5>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TeamCard

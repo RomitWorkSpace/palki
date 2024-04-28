21f66e28e104c9f68873b828648ad4d82d489c6b
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-28 17:17:56 +0530

    Testimonial section created

---
+++ resources/js/components/HomeComponents/TestimonialSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TestimonialSlider from './TestimonialSlider'
+
+function TestimonialSection() {
+    return (
+        <>
+        <div className='container-fluid wrapper-bg pt-8 pb-5'>
+                <h4 className='clr-palki bold-7 text-center'>Our Testimonials</h4>
+                <h2 className='bold-9 text-center'>What Our Customers Says!</h2>
+            <div className='container pt-5 pb-4'>
+                <TestimonialSlider />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default TestimonialSection

a82856542dc586ffc76f998c3b2cc7a11c52395b
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-28 17:14:30 +0530

    testimonial card created

---
+++ resources/js/components/HomeComponents/TestimonialCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function TestimonialCard() {
+    return (
+        <>
+        <div className='testimonial-wrapper p-4'>
+            <div className='testimonial-inner'>
+                <div className='row align-items-center'>
+                    <img src="https://kamleshyadav.com/html/catering/html/assets/images/main/team/01.jpg" alt="waiter" />
+                    <div className='testimonial-title'>
+                        <h4 className='bold-7'>John Brown</h4>
+                        <p className='clr-palki'>BPO Manager</p>
+                    </div>
+                    <p className='text-justify mt-4'>There cursus massa at urnaaculis estie. Sed aliquamellus vitae ultrs condmentum leo massa mollis estiegittis miristum nulla sed medy fringilla vitae</p>
+    
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default TestimonialCard

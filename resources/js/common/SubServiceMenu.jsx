87296559a0c3321536b0cb83c110d630409c42ad
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-29 16:13:22 +0530

    navbar sub service menu created

---
+++ resources/js/common/SubServiceMenu.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+function SubServiceMenu({ props }){
+    const [submenu, setSubMenu] = useState([]);
+    
+    useEffect(() => {
+
+        axios.get(`https://filmcityinfo.com/api/subservices/${props}`).then(res=>{
+            if(res.status === 200)
+            {
+                setSubMenu(res.data.subservices)
+                // setLoading(false);
+            }
+        });
+
+    }, []);
+
+    var subserviceMenu = "";
+
+    subserviceMenu = submenu.map( (item, index) => {
+        return (
+            <>
+                  <li onClick={() => handleNav()}><Link to={ `/service/${item.slug}` }>{item.title}</Link></li>
+            </>
+        )
+    })
+
+    return(
+        <>
+            { subserviceMenu }
+        </>
+    );
+
+}
+
+export default SubServiceMenu;

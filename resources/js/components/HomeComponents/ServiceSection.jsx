96e7ce597f1e55e99d62e95b9fa2ddda9558aca4
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-01 18:13:19 +0530

    Service icon added

--- resources/js/components/HomeComponents/ServiceSection.jsx
+++ resources/js/components/HomeComponents/ServiceSection.jsx
@@ -2,26 +2,31 @@ import React from 'react'
 import ServiceCard from './ServiceCard'
 import { Link } from 'react-router-dom'
 
+import EventIcon from 'imgPath/event-catering.png'
+import CorporateIcon from 'imgPath/Corporate-Catering.png'
+import BakeryIcon from 'imgPath/Industrial-Bakery.png'
+
 function ServiceSection() {
+    const EventService = {icon: EventIcon, name: 'Event Catering'}
+    const CorporateService = {icon: CorporateIcon, name: 'Corporate Catering'}
+    const BakeryService = {icon: BakeryIcon, name: 'Industrial Bakery'}
     return (
         <>
         <div className='container-fluid wrapper-bg pt-8 pb-5'>
             <h4 className='text-center clr-palki bold-7'>Our Services</h4>
             <h2 className='text-center bold-9'>What We Offer</h2>
             <div className='container pt-5'>
-                <div className='row'>
+                <div className='row' style={{justifyContent:'space-around'}}>
                     <div className='col-md-3 mb-4'>
-                        <ServiceCard />
+                        <ServiceCard props = {CorporateService} />
                     </div>
                     <div className='col-md-3 mb-4'>
-                        <ServiceCard />
+                        <ServiceCard props = { BakeryService } />
                     </div>
                     <div className='col-md-3 mb-4'>
-                        <ServiceCard />
+                        <ServiceCard props = { EventService } />
                     </div>
-                    <div className='col-md-3 mb-4'>
-                        <ServiceCard />
-                    </div>
+                    
                 </div>
                 <div className='text-center'>
                     <Link to="/services">

af6cbd159b8fa0836faf48f46183518e7199d70f
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-01 18:09:08 +0530

    Link added to footer menu

--- resources/js/common/Footer.jsx
+++ resources/js/common/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from 'imgPath/PalkiLogo.png'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
@@ -10,48 +11,45 @@ function Footer() {
     <div className="container">
       <div className="row gy-4">
         <div className="col-lg-4 col-md-12 footer-info">
-          <a href="index.html" className="logo d-flex align-items-center">
+          <Link to="index.html" className="logo d-flex align-items-center">
             <img src={Logo} alt="tecroost" height="180" />
-          </a>
-          <p className='clr-black text-justify'>Palki Food Services is providing Catering services to employees on daily basis in different industries, Palki Food Services offers multitude of outsourced services for its clients across sectors and geographical areas.</p>
+          </Link>
+          <p className='clr-black text-justify'>Palki Food Services is providing Catering services to employees on daily basis in different industries.</p>
           <div className="social-links d-flex mt-4">
-            <a href="#" className="twitter"><i className="bi bi-twitter clr-palki"></i></a>
-            <a href="#" className="facebook"><i className="bi bi-facebook clr-palki"></i></a>
-            <a href="#" className="instagram"><i className="bi bi-instagram clr-palki"></i></a>
-            <a href="#" className="linkedin"><i className="bi bi-linkedin clr-palki"></i></a>
+            <Link to="#" className="twitter"><i className="bi bi-twitter clr-palki"></i></Link>
+            <Link to="https://www.facebook.com/pg/PalkiFoodServices/posts/" className="facebook"><i className="bi bi-facebook clr-palki"></i></Link>
+            <Link to="#" className="instagram"><i className="bi bi-instagram clr-palki"></i></Link>
+            <Link to="https://www.linkedin.com/company/palki-foods-india-pvt-ltd-/?originalSubdomain=in" className="linkedin"><i className="bi bi-linkedin clr-palki"></i></Link>
           </div>
         </div>
 
         <div className="col-lg-2 col-6 footer-links">
           <h4 className="footer-title">Useful Links</h4>
           <ul className='footer-link'>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Our Services</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contact us</a></li>
+            <li><Link to="">Home</Link></li>
+            <li><Link to="/about">About us</Link></li>
+            <li><Link to="/services">Our Services</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
           </ul>
         </div>
 
         <div className="col-lg-3 col-6 footer-links">
           <h4 className="footer-title">Our Services</h4>
           <ul className='footer-link'>
-            <li><a href="#">Industrial Cafeteria Meals</a></li>
-            <li><a href="#">Corporate Cafeteria Meals</a></li>
-            <li><a href="#">Bulk Bakery Supplies</a></li>
-            <li><a href="#">Corporate Supplies</a></li>
-            <li><a href="#">Event Catering</a></li>
+            <li><Link to="/service/industrial-cafeteria-meals">Industrial Cafeteria</Link></li>
+            <li><Link to="/service/corporate-cafeteria-meals">Social Event Catering</Link></li>
+            <li><Link to="/service/bulk-bakery-supplies">Bulk Bakery Supplies</Link></li>
+            <li><Link to="/service/corporate-supplies">Corporate Supplies</Link></li>
+            
           </ul>
         </div>
 
         <div className="col-lg-3 col-md-12 footer-contact text-center text-md-start">
           <h4 className="footer-title">Contact Us</h4>
           <p className='clr-black'>
-          Plot No. 6, Sector 21,
-Greater Noida, Gautam Budh Nagar,
-UP - 201308 <br/><br/>
-            <strong>Phone:</strong> +91-8178126122<br/>
-            <strong>Email:</strong> admin@palki.in<br/>
+          Plot no. 228 Udhyog Kendra 1st Ecotech 3, Extension, Noida <br/><br/>
+            <strong>Phone:</strong> + 91-8860619770 / 71<br/>
+            <strong>Email:</strong> info@palkifoods.co.in<br/>
           </p>
 
         </div>
@@ -59,13 +57,13 @@ UP - 201308 <br/><br/>
       </div>
     </div>
 
-    <div className="container-fluid mt-4 bg-palki footer-copyright p-3">
+    <div className="container-fluid bg-palki footer-copyright p-3">
       <div className="copyright clr-white">
         &copy; Copyright <strong><span className=''>Palki Foods</span></strong>. All Rights Reserved
       </div>
       <div className="credits clr-white">
 
-        Designed by <a href="https://livetechservices.in" className="">LTS</a>
+        Designed by <Link to="https://livetechservices.in" className="">LTS</Link>
       </div>
     </div>
 

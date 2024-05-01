6729fbcabd966e7dc02483b9ff11e97518dd14e8
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-01 18:20:13 +0530

    Dynamic data display on service page

--- resources/js/components/pages/Service.jsx
+++ resources/js/components/pages/Service.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
-import EventImg from 'imgPath/event.jpeg'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import GallerySection from '../HomeComponents/GallerySection'
 import PageBanner from '../../common/PageBanner'
 import mbanner from 'imgPath/banners/Services-banner-mb.jpg'
 import dbanner from 'imgPath/banners/Services-banner.jpg'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 function Service() {
+
+    const [allservices, setAllServices] = useState([]);
+
+    useEffect(() => {
+
+        axios.get(`/api/allservices`).then(res=>{
+            if(res.status === 200)
+            {
+                setAllServices(res.data.services)
+                // setLoading(false);
+            }
+        });
+
+    }, []);
+    
+    var serviceData ="";
+    serviceData = allservices.map( (item, index) => {
+        return (
+            <>
+            {item.id % 2 == 0 ?
+             <>
+             <div key={item.id} className='container pt-5'>
+                <div className='row align-items-center column-reverse'>
+                    
+                    <div className='col-md-6 text-center'>
+                        <h4 className='event-text'>{item.name}</h4>
+                        <hr className='line' />
+                        <div className='event-info mt-4'>
+                            <div dangerouslySetInnerHTML={{ __html: item.short_desc }} />
+                        </div>
+                        <Link to={`/service/${item.slug}`}>
+                        <button className='btn-palki'>Explore Now</button>
+                        </Link>
+                    </div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-5 mb-4'>
+                        <img src={`https://filmcityinfo.com/public/images/services/${item.image}`} alt="event" width="100%" style={{borderRadius:'10px'}} />
+                    </div>
+                </div>
+            </div>
+             </>:<>
+             <div key={item.id} className='container pt-5'>
+                <div className='row align-items-center'>
+                    <div className='col-md-5 mb-4'>
+                        <img src={`https://filmcityinfo.com/public/images/services/${item.image}`} alt="event" width="100%" style={{borderRadius:'10px'}} />
+                    </div>
+                    <div className='col-md-1'></div>
+                    <div className='col-md-6 text-center'>
+                        <h4 className='event-text'>{item.name}</h4>
+                        <hr className='line' />
+                        <div className='event-info mt-4'>
+                            <div dangerouslySetInnerHTML={{ __html: item.short_desc }} />
+                        </div>
+                        <Link to={`/service/${item.slug}`}>
+                        <button className='btn-palki'>Explore Now</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+             </>}
+            </>
+        )
+    })
 
     const Banners = {mobile_banner : mbanner, desktop_banner : dbanner}
     return (
@@ -14,42 +77,9 @@ function Service() {
         <PageBanner props = {Banners}/>
         <div className='container-fluid pt-5 pb-5'>
             <h4 className='text-center clr-palki bold-7'>Our Services</h4>
-            <div className='container pt-5'>
-                <div className='row align-items-center'>
-                    <div className='col-md-5 mb-4'>
-                        <img src={ EventImg } alt="event" width="100%" style={{borderRadius:'10px'}} />
-                    </div>
-                    <div className='col-md-1'></div>
-                    <div className='col-md-6 text-center'>
-                        <h4 className='event-text'>SPECIAL EVENTS</h4>
-                        <hr className='line' />
-                        <div className='event-info mt-4'>
-                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor iesdein reprehendeerit in voluptate velit esse cillum dolore.</p>
-                        </div>
-                        <Link to="/service-detail">
-                        <button className='btn-palki'>Explore Now</button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            <div className='container pt-5'>
-                <div className='row align-items-center column-reverse'>
-                    <div className='col-md-6 text-center mb-4'>
-                        <h4 className='event-text'>CORPORATE EVENTS</h4>
-                        <hr className='line' />
-                        <div className='event-info mt-4'>
-                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor iesdein reprehendeerit in voluptate velit esse cillum dolore.</p>
-                        </div>
-                        <Link to="/service-detail">
-                        <button className='btn-palki'>Explore Now</button>
-                        </Link>
-                    </div>
-                    <div className='col-md-1'></div>
-                    <div className='col-md-5 mb-4'>
-                        <img src={ EventImg } alt="event" width="100%" style={{borderRadius:'10px'}} />
-                    </div>
-                </div>
-            </div>
+
+                { serviceData }
+
         </div>
         <GallerySection />
         </>

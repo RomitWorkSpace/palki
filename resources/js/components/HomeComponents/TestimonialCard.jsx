1fb8ffbf40e4f6a28bb292ae6a95b9e59d27c8d0
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-03 19:00:57 +0530

    component enable to support dynamic contents

--- resources/js/components/HomeComponents/TestimonialCard.jsx
+++ resources/js/components/HomeComponents/TestimonialCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TestimonialCard() {
+function TestimonialCard({props}) {
     return (
         <>
         <div className='testimonial-wrapper p-4'>
@@ -8,10 +8,10 @@ function TestimonialCard() {
                 <div className='row align-items-center'>
                     <img src="https://kamleshyadav.com/html/catering/html/assets/images/main/team/01.jpg" alt="waiter" />
                     <div className='testimonial-title'>
-                        <h4 className='bold-7'>John Brown</h4>
-                        <p className='clr-palki'>BPO Manager</p>
+                        <h4 className='bold-7'>{props.name}</h4>
+                        <p className='clr-palki'>{props.cname}</p>
                     </div>
-                    <p className='text-justify mt-4'>There cursus massa at urnaaculis estie. Sed aliquamellus vitae ultrs condmentum leo massa mollis estiegittis miristum nulla sed medy fringilla vitae</p>
+                    <p className='text-justify mt-4'>{props.quote}</p>
     
                 </div>
             </div>

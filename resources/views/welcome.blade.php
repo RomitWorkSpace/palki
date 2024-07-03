1568dbdbf5f52b6593b6e8e099f633d93cda4794
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-03 20:48:31 +0530

    Social media icon added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,13 @@
     width: 80px;
     z-index: 9999;
 }
+.social-media-bg{
+    position: fixed;
+    bottom: 40%;
+    right: 0;
+    width: 35px;
+    z-index: 999;
+}
       </style>
     </head>
     <body style="">
@@ -38,6 +45,20 @@
         <a href="https://wa.me/+918860619770">
             <img src="whatsapp_icon.png" width="100%" alt="whatsapp" />
         </a>
+    </div>
+    <div class="social-media-bg">
+        <div class="instagram">
+            <img src="/images/insta.png" alt="instagram" width="100%" />
+        </div>
+        <div class="facebook">
+            <img src="/images/facebook.png" alt="instagram" width="100%" />
+        </div>
+        <div class="twitter">
+            <img src="/images/x.png" alt="instagram" width="100%" />
+        </div>
+        <div class="linkedin">
+            <img src="/images/linkedin.png" alt="instagram" width="100%" />
+        </div>
     </div>
     <div id="app"></div>
         @viteReactRefresh

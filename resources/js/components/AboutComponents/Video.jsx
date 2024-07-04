f0deaccac573a2952e1dd66782adfc0162a66221
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-04 18:57:15 +0530

    Video added and dynamic counter implement

--- resources/js/components/AboutComponents/Video.jsx
+++ resources/js/components/AboutComponents/Video.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CountUp from 'react-countup'
+
 import DiwaliIcon from 'imgPath/Diwali-Mela.png'
 import HoliIcon from 'imgPath/holi.png'
 import MusicalIcon from 'imgPath/Musical.png'
@@ -7,18 +9,19 @@ import FamilyIcon from 'imgPath/family.png'
 
 function Video() {
     return (
-        <>
+        <><hr/>
         <div className='container pt-5 pb-5'>
             <div className='row align-items-center'>
                 <div className='col-md-5'>
                     <div className='video-wrapper'>
-                        <img src="https://kamleshyadav.com/html/catering/html/assets/images/main/banner-bg.jpg" alt="video" width="100%" />
+                        {/* <img src="https://kamleshyadav.com/html/catering/html/assets/images/main/banner-bg.jpg" alt="video" width="100%" />
                         <div className='play-btn'>
                             <Link to="/" className='popup-youtube'>
                                 <i className='fa fa-play'></i>
                             </Link>
                             <div className='sonar-wave'></div>
-                        </div>
+                        </div> */}
+                        <video class="elementor-video" src="/public/Palki-Food-Full-480-1.mp4" autoplay="" loop="" muted="muted" playsinline="" controlslist="nodownload" style={{width:'100%'}}></video>
                     </div>
                 </div>
                 <div className='col-md-7'>
@@ -27,28 +30,36 @@ function Video() {
                             <div className='col-md-3 mb-4'>
                                 <div className='counter-inner'>
                                     <img src={FamilyIcon} alt="counter" width="100%" />
-                                    <h4 className=''>12,000+</h4>
+                                    <h4 className='clr-palki'>
+                                        <CountUp start={0} end={12000} duration={4} scrollSpyOnce />+
+                                    </h4>
                                     <p>Pax Family Day</p>
                                 </div>
                             </div>
                             <div className='col-md-3 mb-4'>
                                 <div className='counter-inner'>
                                     <img src={DiwaliIcon} alt="counter" width="100%" />
-                                    <h4 className=''>6,000+</h4>
+                                    <h4 className='clr-palki'>
+                                        <CountUp start={0} end={6000} duration={4} scrollSpyOnce />+
+                                    </h4>
                                     <p>Pax Diwali Mela</p>
                                 </div>
                             </div>
                             <div className='col-md-3 mb-4'>
                                 <div className='counter-inner'>
                                     <img src={MusicalIcon} alt="counter" width="100%" />
-                                    <h4 className=''>3,000+</h4>
+                                    <h4 className='clr-palki'>
+                                        <CountUp start={0} end={3000} duration={4} scrollSpyOnce />+
+                                    </h4>
                                     <p>Musical Concert</p>
                                 </div>
                             </div>
                             <div className='col-md-3 mb-4'>
                                 <div className='counter-inner'>
                                     <img src={HoliIcon} alt="counter" width="100%" />
-                                    <h4 className=''>1,200+</h4>
+                                    <h4 className='clr-palki'>
+                                        <CountUp start={0} end={1200} duration={4} scrollSpyOnce />+
+                                    </h4>
                                     <p>Holi Party</p>
                                 </div>
                             </div>
@@ -56,7 +67,7 @@ function Video() {
                     </div>
                 </div>
             </div>
-        </div><hr/>
+        </div>
         </>
     )
 }

44b584b9b230eec6ea40d224be083b8af763d2c8
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-04 19:01:23 +0530

    User icon updated

--- resources/js/components/HomeComponents/TestimonialCard.jsx
+++ resources/js/components/HomeComponents/TestimonialCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import User from 'imgPath/user.png'
+
 function TestimonialCard({props}) {
     return (
         <>
         <div className='testimonial-wrapper p-4'>
             <div className='testimonial-inner'>
                 <div className='row align-items-center'>
-                    <img src="https://kamleshyadav.com/html/catering/html/assets/images/main/team/01.jpg" alt="waiter" />
+                    <img src={ User } alt="waiter" className='' />
                     <div className='testimonial-title'>
                         <h4 className='bold-7'>{props.name}</h4>
                         <p className='clr-palki'>{props.cname}</p>

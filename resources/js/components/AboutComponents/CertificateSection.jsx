c753cd08709be26a21ae97f6c47f6f7a90fb0301
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-05 19:17:21 +0530

    title font changed

--- resources/js/components/AboutComponents/CertificateSection.jsx
+++ resources/js/components/AboutComponents/CertificateSection.jsx
@@ -13,7 +13,7 @@ function CertificateSection() {
         <>
         <div className='container-fluid pt-8 pb-5'>
                 <div className='' data-aos="fade-down">
-                    <h2 className='bold-9 text-center clr-palki sm-text-shadow dancing'>Recoginition and Certificate</h2>
+                    <h1 className='bold-9 text-center clr-palki sm-text-shadow font-charm'>Recoginition and Certificate</h1>
                 </div>
             <div className='container pt-5 pb-4'>
                 <CertificateSlider />

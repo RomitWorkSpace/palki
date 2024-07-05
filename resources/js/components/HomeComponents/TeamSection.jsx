ff7f2e5d53c61fb1a8f744a391e0fe4c1f75683e
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-05 19:15:55 +0530

    Background image added and title font changed

--- resources/js/components/HomeComponents/TeamSection.jsx
+++ resources/js/components/HomeComponents/TeamSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TeamSlider from './TeamSlider'
+import PillarBg from 'imgPath/our-pillars-bg.jpg'
 
 function TeamSection() {
     return (
         <>
-        <div className='container-fluid pt-8 pb-5'>
-            <h4 className='clr-palki text-center bold-7'>Our Team</h4>
-            <h2 className='text-center bold-9'>We Have Experienced Team</h2>
+        <div className='container-fluid our-team-bg pt-8 pb-5' style={{backgroundImage: `url(${ PillarBg })`}}>
+            
+            <h1 className='clr-palki text-center bold-7 sm-text-shadow font-charm'>Founding Pillars</h1>
+            <h6 className='text-center bold-9'>Our Skilled Team Sets the Standard</h6>
 
             <TeamSlider />
         </div>

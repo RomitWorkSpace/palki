c888e96badc784d7355b6ee8cda4afd35adf2797
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-05 19:14:46 +0530

    scroll animation added and title font changed

--- resources/js/components/HomeComponents/TestimonialSection.jsx
+++ resources/js/components/HomeComponents/TestimonialSection.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TestimonialSlider from './TestimonialSlider'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function TestimonialSection() {
+
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <>
-        <div className='container-fluid wrapper-bg pt-8 pb-5'>
-                <h4 className='clr-palki bold-7 text-center'>Our Testimonials</h4>
-                <h2 className='bold-9 text-center'>What Our Customers Say!</h2>
+        <div className='container-fluid testimonial-bg pt-8 pb-5'>
+                <div className='' data-aos="fade-down">
+                    <h1 className='clr-palki bold-7 text-center sm-text-shadow font-charm'>Testimonials</h1>
+                    <h6 className='bold-9 text-center'>What Our Clients Say About Us!</h6>
+                </div>
             <div className='container pt-5 pb-4'>
                 <TestimonialSlider />
             </div>

8db0e726cffe715a777a810f9f0bf324c5d9c5df
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-24 12:57:32 +0530

    Breadcrumb component imported in Booking page

--- resources/js/components/pages/Booking.jsx
+++ resources/js/components/pages/Booking.jsx
@@ -4,12 +4,14 @@ import GallerySection from '../HomeComponents/GallerySection'
 import mbanner from 'imgPath/banners/Share-banner-mb.jpg'
 import dbanner from 'imgPath/banners/Share-banner.jpg'
 import PageBanner from '../../common/PageBanner'
+import Breadcrumb from '../../common/Breadcrumb'
 
 function Booking() {
     const Banners = {mobile_banner : mbanner, desktop_banner : dbanner}
     return (
         <>
         <PageBanner props = {Banners} />
+        <Breadcrumb page='Share Query' />
         <BookingSection />
         <GallerySection />
         </>

69177a306ef01d2e7f215cde4560ac33ff59e8f2
RomitWorkSpace <romitkumar848@gmail.com>
2024-08-19 15:13:14 +0530

    meta data added to this page

--- resources/js/components/pages/Training.jsx
+++ resources/js/components/pages/Training.jsx
@@ -7,6 +7,7 @@ import Breadcrumb from '../../common/Breadcrumb'
 import Slogan from '../TrainingComponents/Slogan'
 import AllTraining from '../TrainingComponents/AllTraining'
 import TrainingSlider from '../TrainingComponents/TrainingSlider'
+import OnPage from '../../common/OnPage'
 
 
 function Training() {
@@ -14,6 +15,10 @@ function Training() {
     const Banners = {mobile_banner : mbanner, desktop_banner : dbanner}
     return (
         <>
+        <OnPage meta_title="Training, Learning & Growing - Palki Food Services"
+        keywords = "Palki Food Services training, Food service industry training, Palki Foods learning and growth, Professional development in food service, Corporate training Palki Foods, Learning programs Palki Food Services, Learning opportunities Palki Foods"
+        meta_desc ="Palki Foods: Expert training in operations and development. Enhance skills and efficiency with our specialized training programs tailored to your needs."
+        />
         <PageBanner props = {Banners} />
         <Breadcrumb page='Training, learning & growing' />
         <Slogan />

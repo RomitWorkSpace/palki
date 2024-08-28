fcb0b9d8d12f3cf49bd708b375eb8201c07701df
RomitWorkSpace <romitkumar848@gmail.com>
2024-08-28 22:26:33 +0530

    About us meta data added

--- resources/js/components/pages/Home.jsx
+++ resources/js/components/pages/Home.jsx
@@ -10,10 +10,15 @@ import TestimonialSection from '../HomeComponents/TestimonialSection'
 import ClientSection from '../HomeComponents/ClientSection'
 import MainSlider from '../../common/MainSlider'
 import Video from '../AboutComponents/Video'
+import OnPage from '../../common/OnPage'
 
 function Home() {
     return (
         <>
+        <OnPage meta_title="Premier catering Service Provider for Corporate, Industrial"
+        keywords = "Palki Food, Food Service Company, Palki Food Services, Industrial Cafeteria Meals,Corporate Cafeteria Meals, Bulk Bakery Supplies, Corporate Supplies, Catering for Social Ceremonies, Catering for Large Corporate"
+        meta_desc = "Palki Food Services: Premier catering for corporate, industrial, and MNCs. Tailored solutions for diverse tastes and seamless dining experiences."
+        />
         <MainSlider />
         <StorySection />
         <Video />

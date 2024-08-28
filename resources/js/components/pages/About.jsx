d046a6facdcc7e274b13c984810e32a41d4bcfcc
RomitWorkSpace <romitkumar848@gmail.com>
2024-08-28 22:25:21 +0530

    About us meta data added

--- resources/js/components/pages/About.jsx
+++ resources/js/components/pages/About.jsx
@@ -8,11 +8,16 @@ import VisionSection from '../AboutComponents/VisionSection'
 import AboutCompany from '../AboutComponents/AboutCompany'
 import CertificateSection from '../AboutComponents/CertificateSection'
 import Breadcrumb from '../../common/Breadcrumb'
+import OnPage from '../../common/OnPage'
 
 function About() {
     const Banners = {mobile_banner : mbanner, desktop_banner : dbanner}
     return (
         <>
+        <OnPage meta_title = "About Us| Palki Food Services - Food Service Company"
+        keywords = "About Palki Food Services, Palki Food Services company overview, Food service company Noida, Palki Foods mission and values, Who we are Palki Food Services, Palki Foods team and expertise, Why choose Palki Food Services"
+        meta_desc = "Palki Food Services, your premier provider of catering solutions for various industries including corporate, industrial, and MNCs."
+         />
         <PageBanner props = {Banners}/>
         <Breadcrumb page= 'About' />
         <StorySection />

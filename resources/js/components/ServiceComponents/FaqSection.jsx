b4bd753d8e736962408578cb91ddd368d28292ed
RomitWorkSpace <romitkumar848@gmail.com>
2024-09-04 22:58:53 +0530

    FAQ section completed

--- resources/js/components/ServiceComponents/FaqSection.jsx
+++ resources/js/components/ServiceComponents/FaqSection.jsx
@@ -5,7 +5,7 @@ function FaqSection() {
     return (
         <>
         <div className='container-fluid pt-5 pb-5'>
-            <h2 className='clr-palki text-center'>Frequently Asked Questions</h2>
+            <h2 className='clr-palki text-center font-charm'>Frequently Asked Questions</h2>
             <div className='container pt-4'>
                 <div className='row'>
                     <div className='col-md-2'></div>
